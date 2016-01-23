0ae4fe7c9f14799d1e558a112999fd95db05639c
yanovskyy <vadim.janovskij@mail.muni.cz>
2016-01-23 23:41:26 +0100

    Add reporter.py to nmap_test and ssh_authentication_test

---
+++ external_tests/ssh_authentication_test/reporter.py
@@ -0,0 +1,18 @@
+import re
+import fileinput
+from lxml import etree
+
+ssh_auth_test =  etree.Element('SSH_AUTH_TEST')
+allowed = False
+
+for line in fileinput.input():
+    regex = re.search('Permission\sdenied\s[(][a-z,-]+[,]password[a-z,)].*', line)
+    if regex:
+        ssh_auth_test.text = "SSH password authentication is allowed."
+        allowed = True
+        break
+
+if not allowed:
+    ssh_auth_test.text = "SSH password authentication is not allowed."
+
+print etree.tostring(ssh_auth_test,pretty_print=True)
