2d7984151761e700cba8e0f37591ad634b61d22c
Daniel Kouril <kouril@ics.muni.cz>
2017-10-03 14:43:04 +0200

    user proper directive name

--- cron_scripts/generate_token.sh
+++ cron_scripts/generate_token.sh
@@ -3,5 +3,5 @@
 CONFIG_DIR=${SECANT_CONFIG_DIR:-/etc/secant}
 source ${CONFIG_DIR}/secant.conf
 export ONE_XMLRPC=$ONE_XMLRPC
-oneuser login secant --cert $CERT_PATH --key $CERT_KEY --x509 --force
+oneuser login secant --cert "$CERT_PATH" --key "$KEY_PATH" --x509 --force
 logger -s "Token generated!"
