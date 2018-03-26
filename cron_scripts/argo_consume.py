25993f644d714d8d45b447c3a444ffedba081aff
Daniel Kouril <kouril@ics.muni.cz>
2018-03-26 16:52:09 +0200

    Use LOG_DIR to keep the output of cloudkeeper
    Current implementation of cloudkeeper doesn't provide status code, let's keep
    the whole debug output for potential investigations later.

--- cron_scripts/argo_consume.py
+++ cron_scripts/argo_consume.py
@@ -21,12 +21,16 @@ if __name__ == "__main__":
     url = py_functions.getSettingsFromBashConfFile(secant_conf_path, "IMAGE_LIST_URL")
     dir = py_functions.getSettingsFromBashConfFile(secant_conf_path, "IMAGE_LIST_DIR")
     state_dir = py_functions.getSettingsFromBashConfFile(secant_conf_path, "STATE_DIR")
+    log_dir = py_functions.getSettingsFromBashConfFile(secant_conf_path, "LOG_DIR")
+
     registered_dir = state_dir + "/registered"
     if not os.path.isdir(registered_dir):
         os.mkdir(registered_dir, 755)
 
     images = argo.get_templates_for_assessment(dir)
     logging.info("Secant consumer: Obtained %d image list(s) for assessment" % (len(images)))
+
+    cloudkeeper_log = open(log_dir + "/cloudkeeper.log", mode="a");
 
     for img_list in images:
         #sudo -u cloudkeeper /opt/cloudkeeper/bin/cloudkeeper --image-lists=https://vmcaster.appdb.egi.eu/store/vappliance/demo.va.public/image.list --debug
@@ -35,10 +39,12 @@ if __name__ == "__main__":
         img_url = "%s/%s" % (url, img_list)
         cmd = (["sudo", "-u", "cloudkeeper", "/opt/cloudkeeper/bin/cloudkeeper", "--image-lists=" + img_url, "--debug"])
         try:
-            subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+            subprocess.check_call(cmd, stdout=cloudkeeper_log, stderr=subprocess.STDOUT)
         except subprocess.CalledProcessError as e:
             logging.error("Secant consumer: Registering image list %s failed: %s" % (img_list, e.output))
             print("Failed to register image %s, check the log." % img_list, file=sys.stderr)
             continue
         logging.debug("Secant consumer: Image list %s has been registered" % (img_list))
         os.rename("%s/%s" % (dir, img_list), "%s/%s" % (registered_dir, img_list))
+
+    cloudkeeper_log.close()
