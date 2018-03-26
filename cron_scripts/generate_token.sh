f071de4fa22b22e1657852e41403bf3d79f27f93
Daniel Kouril <kouril@ics.muni.cz>
2018-03-26 17:15:03 +0200

    Improve the script to generate ON AuthN tokens

--- cron_scripts/generate_token.sh
+++ cron_scripts/generate_token.sh
@@ -3,5 +3,12 @@
 CONFIG_DIR=${SECANT_CONFIG_DIR:-/etc/secant}
 source ${CONFIG_DIR}/secant.conf
 export ONE_XMLRPC=$ONE_XMLRPC
-oneuser login secant --cert "$CERT_PATH" --key "$KEY_PATH" --x509 --force
-logger -s "Token generated!"
+
+ret=$(oneuser login secant --cert "$CERT_PATH" --key "$KEY_PATH" --x509 --force 2>&1)
+if [ $? -ne 0 ]; then
+    echo $ret
+    exit 1
+fi
+cp ~/.one/one_auth ~cloudkeeper-one/.one && (chown cloudkeeper-one ~cloudkeeper-one/.one/one_auth; chmod 0600 ~cloudkeeper-one/.one/one_auth)
+#systemctl restart cloudkeeper-one
+/etc/init.d/cloudkeeper-one restart > /dev/null
