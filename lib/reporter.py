ccf60e9e79b8b2db8cbf7918caf23314e8790134
Daniel Kouril <kouril@ics.muni.cz>
2018-04-05 09:32:04 +0200

    Fix the XML format produced

--- lib/reporter.py
+++ lib/reporter.py
@@ -8,14 +8,12 @@ name = sys.argv[1]
 status = sys.stdin.readline()
 status = status.rstrip(os.linesep)
 
-print("<%s>" % name)
-print("\t<status=\"%s\" />" % status)
+print("<%s status=\"%s\">" % (name, status))
 
-if status != "SKIP":
-  print("\t<outcome>")
-  for line in sys.stdin:
-      # Escaping, ... !
-      print(line.rstrip(os.linesep))
-  print("\t</outcome>")
+print("\t<outcome>")
+for line in sys.stdin:
+    # Escaping, ... !
+    print(line.rstrip(os.linesep))
+print("\t</outcome>")
 
 print("</%s>" % name)
