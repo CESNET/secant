28fe9b522c68b31f2c054db34d0edb30842e2369
Jakub Havrila <jakub.havrila@gmail.com>
2018-08-02 11:32:53 +0000

    Added prefix for probes configuration variables
    New prefix SECANT_PROBE_ for probes configuration variables.

--- probes/pakiti_test/main.sh
+++ probes/pakiti_test/main.sh
@@ -14,14 +14,14 @@ if [[ $# -eq 0 ]] ; then
     exit 1
 fi
 
-remote_exec "$VM_IP" "$LOGIN_AS_USER" "perl - --site=SECANT" "$PAKITI_CLIENT" "$FOLDER_PATH/pakiti_test-pkgs.txt" 2>$FOLDER_PATH/pakiti.stderr
+remote_exec "$VM_IP" "$LOGIN_AS_USER" "perl - --site=SECANT" "$SECANT_PROBE_PAKITI_CLIENT" "$FOLDER_PATH/pakiti_test-pkgs.txt" 2>$FOLDER_PATH/pakiti.stderr
 if [ $? -ne 0 ]; then
     echo "Pakiti client failed to get a list of installed packages from the VM" >&2
     cat $FOLDER_PATH/pakiti.stderr >&2
     exit 1
 fi
 
-$PAKITI_CLIENT --url "$PAKITI_URL" --mode=store-and-report --host "$TEMPLATE_IDENTIFIER" --input $FOLDER_PATH/pakiti_test-pkgs.txt > $FOLDER_PATH/pakiti_test-result.txt 2>$FOLDER_PATH/pakiti_test-result.stderr
+$SECANT_PROBE_PAKITI_CLIENT --url "$SECANT_PROBE_PAKITI_URL" --mode=store-and-report --host "$TEMPLATE_IDENTIFIER" --input $FOLDER_PATH/pakiti_test-pkgs.txt > $FOLDER_PATH/pakiti_test-result.txt 2>$FOLDER_PATH/pakiti_test-result.stderr
 if [ $? -ne 0 ]; then
     echo "Pakiti test failed while sending data to the Pakiti server" >&2
     cat $FOLDER_PATH/pakiti_test-result.stderr >&2
