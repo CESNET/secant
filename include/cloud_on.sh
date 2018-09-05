57dfe0d4137578cddca06e5b5e9c2c4e6d53bc78
Jakub Havrila <jakub.havrila@gmail.com>
2018-09-05 09:02:43 +0000

    Fixed logging
    Deleted unnecessary logs and fixed formating.

--- include/cloud_on.sh
+++ include/cloud_on.sh
@@ -12,10 +12,8 @@ cloud_init()
 cloud_shutdown_vm()
 {
     VM_ID=$1
-    logging $TEMPLATE_IDENTIFIER "Shutting down VM $VM_ID." "DEBUG"
     onevm shutdown --hard $VM_ID
     if [ $? -ne 0 ]; then
-        logging $TEMPLATE_IDENTIFIER "Failed to shutdown VM $VM_ID." "ERROR"
         return 1
     fi
 }
@@ -24,7 +22,6 @@ cloud_get_vm_ids()
 {
     IDS=$(onevm list)
     if [ $? -ne 0 ]; then
-        logging "Failed to get list of vms ids." "ERROR"
         return 1
     fi
     awk '{ print $1 }' <<< "$IDS" | sed '1d'
@@ -34,7 +31,6 @@ cloud_get_template_ids()
 {
     IDS=$(onetemplate list)
     if [ $? -ne 0 ]; then
-        logging "Failed to get list of templates ids." "ERROR"
         return 1
     fi
     awk '{ print $1 }' <<< "$IDS" | sed '1d'
@@ -46,12 +42,10 @@ cloud_vm_query()
     QUERY=$2
     CLOUD_QUERY=$(onevm show $ID -x)
     if [ $? -ne 0 ]; then
-        logging "Failed to show details of vm with id $ID." "ERROR"
         return 1
     fi
     VM_RESULT=$(xmlstarlet sel -t -v $QUERY -n <<< "$CLOUD_QUERY")
     if [ -z "$VM_RESULT" ]; then
-        logging "Failed to query $QUERY on vm with id $ID." "ERROR"
         return 1
     fi
     printf "$VM_RESULT"
@@ -63,12 +57,10 @@ cloud_template_query()
     QUERY=$2
     CLOUD_QUERY=$(onetemplate show $ID -x)
     if [ $? -ne 0 ]; then
-        logging "Failed to show details of template with id $ID." "ERROR"
         return 1
     fi
     TEMP_RESULT=$(xmlstarlet sel -t -v $QUERY -n <<< "$CLOUD_QUERY")
     if [ -z "$TEMP_RESULT" ]; then
-        logging "Failed to query $QUERY on vm with id $ID." "ERROR"
         return 1
     fi
     printf "$TEMP_RESULT"
@@ -80,7 +72,6 @@ cloud_start_vm()
     CTX=$2
     CLOUD_START_VM=$(onetemplate instantiate $ID $CTX)
     if [ $? -ne 0 ]; then
-        logging "Failed instantiate template $ID." "ERROR"
         return 1
     fi
     echo $CLOUD_START_VM
@@ -91,7 +82,6 @@ cloud_delete_template()
     ID=$1
     CLOUD_DELETE_TEMPLATE=$(onetemplate delete $ID)
     if [ $? -ne 0 ]; then
-        logging "Failed to delete template $ID." "ERROR"
         return 1
     fi
     echo $CLOUD_DELETE_TEMPLATE
@@ -102,7 +92,6 @@ cloud_delete_image()
     IMAGE_NAME=$1
     CLOUD_DELETE_IMAGE=$(oneimage delete $IMAGE_NAME)
     if [ $? -ne 0 ]; then
-        logging "Failed to delete image $IMAGE_NAME." "ERROR"
         return 1
     fi
     echo $CLOUD_DELETE_IMAGE
